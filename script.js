7ad34169374cdfd92edd96462b60f7554a3bf2a8
Afzal Ahmad <113641536+afzalahmad1@users.noreply.github.com>
2023-04-11 14:29:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+for (let i = 0; i < 5; i++) {
+  const randomTime = Math.floor(Math.random() * 5) + 1;
+  promises.push(new Promise((resolve, reject) => {
+    // Insert your code here
+    setTimeout(()=>{
+      resolve(randomTime)
+    },randomTime*1000)
+  }));
+}
+Promise.any(promises).then(result => {
+  document.getElementById("output").innerHTML = `The first promise resolved with ${result} seconds.`;
+});
+
